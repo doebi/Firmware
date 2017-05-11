64c239d4ec1877c23d17d0971a7b61385aab926b
Bas van Sisseren <bas@vansisseren.nl>
2017-05-11 15:57:00 +0200

    only link in all code compatible with the configured eink display.

--- main/demo_partial_update.c
+++ main/demo_partial_update.c
@@ -1,5 +1,6 @@
 #include "sdkconfig.h"
 
+#ifdef CONFIG_SHA_BADGE_EINK_GDEH029A1
 #include <freertos/FreeRTOS.h>
 #include <esp_event.h>
 
@@ -47,3 +48,5 @@ demoPartialUpdate(void) {
 	while ((buttons_down & 0xffff) == 0)
 		xQueueReceive(badge_input_queue, &buttons_down, portMAX_DELAY);
 }
+
+#endif // CONFIG_SHA_BADGE_EINK_GDEH029A1
--- main/main.c
+++ main/main.c
@@ -42,6 +42,7 @@ struct menu_item {
 #include "demo_power.h"
 
 const struct menu_item demoMenu[] = {
+#ifdef CONFIG_SHA_BADGE_EINK_GDEH029A1
     {"text demo 1", &demoText1},
     {"text demo 2", &demoText2},
 #ifdef CONFIG_SHA_BADGE_EINK_GDEH029A1
@@ -52,6 +53,7 @@ const struct menu_item demoMenu[] = {
     {"greyscale image 3", &demoGreyscaleImg3},
 #endif // CONFIG_SHA_BADGE_EINK_GDEH029A1
     {"greyscale image 4", &demoGreyscaleImg4},
+#ifdef CONFIG_SHA_BADGE_EINK_GDEH029A1
     {"partial update test", &demoPartialUpdate},
 #ifdef CONFIG_SHA_BADGE_EINK_GDEH029A1
     {"dot 1", &demoDot1},
